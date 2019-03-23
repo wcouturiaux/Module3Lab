d10eba63d92437ac1cb8ef49e844404a586edbb9
Will C <2willaddiction@gmail.com>
2019-03-23 18:24:47 -0400

    Initial Commit

---
+++ Module3Lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Module3Lab
+//
+//  Created by William Couturiaux on 3/23/19.
+//  Copyright © 2019 William Couturiaux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
