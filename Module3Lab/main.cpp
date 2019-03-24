f4045d88e1f35aa908d3df0ab28379557d0e3d2c
Will C <2willaddiction@gmail.com>
2019-03-24 01:01:03 -0400

    Instantiate a student object.

--- Module3Lab/main.cpp
+++ Module3Lab/main.cpp
@@ -7,9 +7,13 @@
 //
 
 #include <iostream>
+#include "Person.hpp"
+#include "Student.hpp"
+#include "Teacher.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Person *Student1 = new Student;
+    
     return 0;
 }
