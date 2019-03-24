6ca7c5f64d8cb096694b412a91b82c10f0823673
Will C <2willaddiction@gmail.com>
2019-03-24 02:00:39 -0400

    Add output age to Person

--- Include/Person.hpp
+++ Include/Person.hpp
@@ -27,5 +27,5 @@ public:
     Person(std::string, int, std::string, std::string);
     virtual ~Person();
     virtual void OutputIdentity()=0;
-    virtual std::string OutputAge(int);
+    virtual std::string OutputAge();
 };
